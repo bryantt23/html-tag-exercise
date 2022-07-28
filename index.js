98ef91f1cad17918aa4272dcdee9dccbecdfc939
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-07-28 13:19:35 -0700

    add class

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-alert('hii');
-
 class Tag {
   constructor(name) {
-    this.name = `<${name}></${name}/>`;
+    this.name = name;
+    this.classNames = [];
   }
   print = () => {
-    return this.name;
+    const classTemp =
+      this.classNames.length === 0
+        ? ''
+        : ` class="${this.classNames.join(' ')}"`;
+    return `<${this.name}${classTemp}></${this.name}>`;
+  };
+  addClass = className => {
+    this.classNames.push(className);
   };
 }
 
 const tag = new Tag('html');
 console.log(tag.print()); //<html></html>
+
+tag.addClass('blue-theme');
+console.log(tag.print()); //<html class="blue-theme main-content"></html>
