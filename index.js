225e10796f47cef98fa569c3163231b32444f1c7
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-07-28 13:28:11 -0700

    works for 1 level

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ class Tag {
   constructor(name) {
     this.name = name;
     this.classNames = [];
+    this.children = [];
   }
   print = () => {
+    let childrenPrinted = '';
+    for (const child of this.children) {
+      childrenPrinted += this.printHelper(child);
+    }
+
     const classTemp =
       this.classNames.length === 0
         ? ''
         : ` class="${this.classNames.join(' ')}"`;
-    return `<${this.name}${classTemp}></${this.name}>`;
+    return `<${this.name}${classTemp}>${childrenPrinted}</${this.name}>`;
   };
+  printHelper = child => {
+    const classTemp =
+      child.classNames.length === 0
+        ? ''
+        : ` class="${child.classNames.join(' ')}"`;
+    return `<${child.name}${classTemp}></${child.name}>`;
+  };
+
   addClass = className => {
     this.classNames.push(className);
+  };
+  appendChild = child => {
+    this.children.push(child);
   };
 }
 
@@ -23,3 +40,11 @@ console.log(tag.print()); //<html class="blue-theme"></html>
 
 tag.addClass('main-content');
 console.log(tag.print()); //<html class="blue-theme main-content"></html>
+
+const bodyTag = new Tag('body');
+const divTag = new Tag('div');
+tag.appendChild(bodyTag);
+// bodyTag.append_child(divTag);
+console.log('nested?  ', tag.print()); //<html class="blue-theme"><body></body></html>'
+
+//'<html class="blue-theme"><body><div></div></body></html>';
